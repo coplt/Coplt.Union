5495e9830dd5d81836e4229015981185876480c8
U2A5F <U2A5F@outlook.com>
2025-03-09 16:42:00 +0800

    tree

---
+++ Tests/Tree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Coplt.Union;
+
+namespace Tests;
+
+[Union]
+public partial class Tree
+{
+    [UnionTemplate]
+    private interface Template
+    {
+        void Tip();
+        void Node(int Value, Tree Left, Tree Right);
+    }
+
+    public static int Sum(Tree Tree)
+    {
+        switch (Tree.Tag)
+        {
+            case Tags.Node:
+                ref var node = ref Tree.Node;
+                return node.Value + Sum(node.Left) + Sum(node.Right);
+            default:
+                return 0;
+        }
+    }
+
+    public static int Sum2(Tree Tree) => Tree switch
+    {
+        // no by ref pattern match
+        { IsNode: true, Node: var node } => node.Value + Sum(node.Left) + Sum(node.Right),
+        _ => 0,
+    };
+}
