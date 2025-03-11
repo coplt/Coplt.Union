f4729b48d2240a62e25f15ff518cfaa69970ddce
U2A5F <U2A5F@outlook.com>
2025-03-11 22:21:17 +0800

    list

---
+++ Tests/List.cs
@@ -0,0 +1,15 @@
+﻿using Coplt.Union;
+using Coplt.Union.Misc;
+
+namespace Tests;
+
+[Union, UnionSymbol(IsReferenceType = MayBool.True)]
+public partial class list<T>
+{
+    [UnionTemplate]
+    private interface Template
+    {
+        void Nil();
+        void Cons(T Item, list<T> Tail);
+    }
+}
